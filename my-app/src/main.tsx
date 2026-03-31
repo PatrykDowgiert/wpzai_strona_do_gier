150749a63589f1fa90a83f8076b2e3a8e8325f29
pwelenjoyer <151506221+pwelenjoyer@users.noreply.github.com>
2026-03-31 22:51:32 +0200

    komentarze

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,12 +5,24 @@ import './index.css'
 import App from './App.tsx'
 import GamePage from './GamePage.tsx'
 
+// Tworzenie głównego korzenia aplikacji React i renderowanie jej w elemencie o id "root"
 createRoot(document.getElementById('root')!).render(
+
+  // StrictMode pomaga wykrywać potencjalne problemy w aplikacji podczas developmentu
   <StrictMode>
+
+    {/* BrowserRouter umożliwia nawigację między stronami bez przeładowania strony */}
     <BrowserRouter>
+
+      {/* Definicja wszystkich ścieżek (routing) w aplikacji */}
       <Routes>
+
+        {/* Strona główna aplikacji */}
         <Route path="/" element={<App />} />
+
+        {/* Strona szczegółów gry, gdzie :id oznacza dynamiczny parametr w URL */}
         <Route path="/game/:id" element={<GamePage />} />
+
       </Routes>
     </BrowserRouter>
   </StrictMode>,
